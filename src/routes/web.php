2427cc718b2a35ad47fc11d3764fc9c115ebd43c
ahmad <ahmad.m@mass4.om>
2023-04-10 15:12:44 +0400

    Trying to add the prompt message 2

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,9 +2,12 @@
 
 use Acaothman\Contact\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
+use Composer\Config;
 
+$config = new Config();
+$url = $config->get('extra.contact-url.default', '/contact');
     
-    Route::get('contact', [ContactController::class, 'index']);
+    Route::get($url, [ContactController::class, 'index']);
     
-    Route::post('contact', [ContactController::class, 'send'])->name('contact');
+    Route::post($url, [ContactController::class, 'send'])->name('contact');
     
