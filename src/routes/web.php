fb7f5cdc04e5f073a1309f9f3286149f5cda5a14
ahmad <ahmad.m@mass4.om>
2023-04-10 15:20:29 +0400

    another try

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,8 +4,12 @@ use Acaothman\Contact\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 use Composer\Config;
 
+// Include the Composer autoloader
+require_once __DIR__ . '/../vendor/autoload.php';
+
 $config = new Config();
-$url = $config->get('extra.contact-url.default', '/contact');
+$url = \Composer\Config::getInstance()->get('extra.contact-url.default', '/contact');
+
     
     Route::get($url, [ContactController::class, 'index']);
     
